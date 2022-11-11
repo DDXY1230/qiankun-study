117cae1845cb9b20f4fa6f7ae73b461203e3529a
DDXY1230 <2502752288@qq.com>
2022-11-11 16:04:46 +0800

    add micro-app

---
+++ qiankun-micro-app3/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+          这是qiankun-micro-app3
+      </header>
+    </div>
+  );
+}
+
+export default App;
