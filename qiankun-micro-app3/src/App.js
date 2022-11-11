98a54abc946dcecb2b2b0fffc6d5a6cde64c3f9c
DDXY1230 <2502752288@qq.com>
2022-11-11 16:23:37 +0800

    add somen code

--- qiankun-micro-app3/src/App.js
+++ qiankun-micro-app3/src/App.js
@@ -5,6 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
           这是qiankun-micro-app3
       </header>
     </div>
