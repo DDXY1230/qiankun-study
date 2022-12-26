b244c4623379d6d4449c33d6131c556d3d80a25d
DDXY1230 <2502752288@qq.com>
2022-12-26 22:26:09 +0800

    canvas

---
+++ web-components/demo-1/component.vue
@@ -0,0 +1,22 @@
+<!--  -->
+<template>
+<div class=''></div>
+</template>
+
+<script>
+
+export default {
+  setup() {
+
+  },
+  beforeMount() {},
+  mounted() {
+
+  },
+  beforeUnmount (){},
+  unmounted() {}
+}
+</script>
+<style lang='scss' scoped>
+
+</style>
